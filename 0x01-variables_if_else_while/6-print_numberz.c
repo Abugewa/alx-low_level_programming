1a3575b437c18fad2d309990984b87733d4dbde8
Jesudamilola <damilolaolugbemiga@gmail.com>
2022-07-01 06:46:30 -0700

    print single digit numbers with only putchar

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - print all single digits with only putchar
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 48; i < 58; i++);
+	{
+		putchar (i);
+	}
+	putchar ('\n');
+	return (0);
+}
