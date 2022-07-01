9b4be9f7c7e988744957ed8af84f069d0bfc915d
Jesudamilola <damilolaolugbemiga@gmail.com>
2022-07-01 09:18:00 -0700

    print all combinations of two 2 digit numbers

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -14,10 +14,10 @@ int main(void)
 		j = 48;
 		while (j < 58)
 		{
-			k =48;
+			k = 48;
 			while (k < 58)
 			{
-				l =48;
+				l = 48;
 				while (l < 58)
 				{
 					ij = (i * 10) + j;
@@ -29,7 +29,7 @@ int main(void)
 						putchar (' ');
 						putchar (k);
 						putchar (l);
-						if (i != 57 || j != 56 || k != 57 || l !=57)
+						if (i != 57 || j != 56 || k != 57 || l != 57)
 						{
 							putchar (','); putchar (' ');
 						}
