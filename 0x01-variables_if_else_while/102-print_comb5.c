b09f94e215f013ac4ab751b519490541a5231127
Jesudamilola <damilolaolugbemiga@gmail.com>
2022-07-01 09:14:58 -0700

    print all combinations of two 2 digit numbers

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -29,10 +29,9 @@ int main(void)
 						putchar (' ');
 						putchar (k);
 						putchar (l);
-						if (i != 57 || j != 56 || k == l !=57)
+						if (i != 57 || j != 56 || k != 57 || l !=57)
 						{
-							putchar (',');
-							putchar (' ');
+							putchar (','); putchar (' ');
 						}
 					}
 					l++;
