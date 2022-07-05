887e093751831e8e63a15028439bd84cc882fd39
Jesudamilola <damilolaolugbemiga@gmail.com>
2022-07-05 10:48:40 -0700

    printing the letters of the alphabetx10

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -6,11 +6,14 @@
  */
 void print_alphabet_x10(void)
 {
-	int j;
+	int i, j;
 
 	for (j = 0; j < 9; j++)
 	{
-		void print_alphabet(void);
+		for (i = 97; i < 123; i++)
+		{
+			_putchar(i);
+		}
 	}
 	_putchar('\n');
 }
