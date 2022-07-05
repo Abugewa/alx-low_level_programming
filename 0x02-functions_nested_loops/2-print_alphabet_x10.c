07669d193dd275d8a991ba18f7c67ceb96df6bba
Jesudamilola <damilolaolugbemiga@gmail.com>
2022-07-05 10:43:14 -0700

    printing the letters of the alphabetx10

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - print 10 times the alphabet
+ *
+ * Return: Always 0 (Success)
+ */
+void print_alphabet_x10(void)
+{
+	int j;
+
+	for (j = 0; j < 9; j++)
+	{
+		void print_alphabet(void);
+	}
+	_putchar('\n');
+}
