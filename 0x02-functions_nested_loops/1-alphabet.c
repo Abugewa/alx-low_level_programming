05013f66b0a95cc5b86c423dddf6f4e1904ac105
Jesudamilola <damilolaolugbemiga@gmail.com>
2022-07-05 10:30:08 -0700

    printing the letters of the alphabet

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,10 +1,10 @@
 #include "main.h"
 /**
- * main - prints all letters of the alphabet
+ * print_alphabet - prints all letters of the alphabet
  *
  * Return: Always 0 (Success)
  */
-void print_alphabet(void);
+void print_alphabet(void)
 {
 	int i;
 
