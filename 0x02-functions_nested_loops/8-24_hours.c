2e90657a440b003d1b773633e3aa88e7b880cd5a
Jesudamilola <damilolaolugbemiga@gmail.com>
2022-07-05 13:37:26 -0700

    prints every minute of the day

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,39 @@
+#include "main.h"
+/**
+ * jack_bauer - prints every minute of the day
+ *
+ * Return: Always 0 (Success)
+ */
+void jack_bauer(void)
+{
+int i, j, k, l = 48;
+
+while (i <= 57)
+	{
+	j = 48;
+	while (j <= 57)
+	{
+	k = 48;
+	while (k <= 57)
+	{
+	l = 48;
+	while (l <= 57)
+	{
+		if (i != 50 || j != 51 || k != 53 || l != 57)
+		{
+			_putchar(i);
+			_putchar(j);
+			_putchar(58);
+			_putchar(k);
+			_putchar(l);
+			_putchar(10);
+		}
+		l++;
+	}
+	k++;
+	}
+	j++;
+	}
+	i++;
+	}
+}
