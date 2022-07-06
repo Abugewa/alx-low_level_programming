e1639ce52df1a4d45e476aa6e8e1c2a22d666366
Jesudamilola <damilolaolugbemiga@gmail.com>
2022-07-05 17:12:39 -0700

    even fibonacci numbers

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+
+/**
+ * main -sum of even fibonacci numbers
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	long int a, b, nxt, sum;
+
+	a = 1;
+	b = 2;
+	nxt = sum = 0;
+	while (nxt <= 40000)
+	{
+		nxt = a + b;
+		a = b;
+		b = nxt;
+		if (a % 2 == 0)
+		{
+			sum += a;
+		}
+	}
+	printf("%ld\n", sum);
+	return (0);
+}
