b0173ed963eb7172796345223e94adb34ea37287
Jesudamilola <damilolaolugbemiga@gmail.com>
2022-07-05 17:34:27 -0700

    first 98 fibonacci numbers

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -7,12 +7,11 @@
  */
 int main(void)
 {
-	int c, boolean, boolean2;
-	long int a, b, nxt, a1, b1, nxt1;
+	int c;
+	long int a, b, nxt;
 
 	a = 1;
 	b = 2;
-	boolean =  boolean2 = 1;
 	printf("%ld, %ld", a, b);
 	for (c = 0; c < 96; c++)
 	{
