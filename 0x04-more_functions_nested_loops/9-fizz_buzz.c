0ce8439ab902a287e61dce0bbb7e002d58f96e57
Jesudamilola <damilolaolugbemiga@gmail.com>
2022-07-07 07:02:50 -0700

    fizzbuzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -14,6 +14,7 @@ int main(void)
 	int i;
 
 	for (i = 1; i <= 100; i++)
+	{
 		if(i % 3 == 0)
 		{
 			printf("Fizz ");
@@ -37,6 +38,7 @@ int main(void)
 				printf("%d ", i);
 			}
 		}
+	}
 	putchar(10);
 	return (0);
 }
