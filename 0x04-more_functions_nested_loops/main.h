dd8773e1976fc2205bfc85ee20c033f9ce240940
Jesudamilola <damilolaolugbemiga@gmail.com>
2022-07-07 04:31:35 -0700

    check for digits

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -2,5 +2,12 @@
 #define _MAIN_H_
 
 int _isupper(int c);
+int _isdigit(int c);                                                                                                                    
+
+int mul(int a, int b);                                                                                                                  
+
+void print_numbers(void);                                                                                                               
+
+void print_most_numbers(void); 
 
 #endif
