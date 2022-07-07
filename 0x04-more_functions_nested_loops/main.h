7e2008ce74f8e6219caa38e3c1e05465a41d3090
Jesudamilola <damilolaolugbemiga@gmail.com>
2022-07-07 04:51:16 -0700

    print numbers except 2 and 4

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -2,10 +2,8 @@
 #define _MAIN_H_
 
 int _isupper(int c);
-int _isdigit(int c);                                                                                                                    
-
-int mul(int a, int b);                                                                                                                  
-void print_numbers(void);
+int _isdigit(int c);
+int mul(int a, int b);                                                          void print_numbers(void);
 int _putchar(char);
 void print_most_numbers(void); 
 
