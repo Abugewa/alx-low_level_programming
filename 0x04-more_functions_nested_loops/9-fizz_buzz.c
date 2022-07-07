9b623331a8acfc4b13bb80e8a7360d9bfcf72e52
Jesudamilola <damilolaolugbemiga@gmail.com>
2022-07-07 08:32:33 -0700

    fizzbuzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,4 +1,4 @@
-#include<stdio.h>
+#include <stdio.h>
 
 /**
  * main - fizzbuzz
@@ -11,26 +11,26 @@ int main(void)
 	int i;
 
 	i = 1;
-	printf ("%d", i);
+	printf("%d", i);
 	for (i = 2; i <= 100; i++)
 	{
 		if ((i % 3 == 0) && (i % 5 == 0))
 	       	{
-			printf (" FizzBuzz");
+			printf(" FizzBuzz");
 		}
 		else if (i % 3 == 0)
 		{
-			printf (" Fizz");
+			printf(" Fizz");
 		}
 		else if (i % 5 == 0)
 		{
-			printf (" Buzz");
+			printf(" Buzz");
 		}
 		else
 		{
-			printf (" %d", i);
+			printf(" %d", i);
 		}
 	}
-	printf ("\n");
+	printf("\n");
 	return (0);
 }
