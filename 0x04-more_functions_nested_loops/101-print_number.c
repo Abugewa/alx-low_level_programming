40841f531a1623585a277d8cb1568b0bfa52d13e
Jesudamilola <damilolaolugbemiga@gmail.com>
2022-07-07 10:31:21 -0700

    print integers

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -8,8 +8,8 @@
  */
 void print_number(int n)
 {
-	unsigned int m, d, count;
-	
+	unsigned int m, d, i;
+
 	if (n < 0)
 	{
 		_putchar(45);
@@ -20,14 +20,14 @@ void print_number(int n)
 		m = n;
 	}
 	d = m;
-	count = 1;
+	i = 1;
 	while (d > 9)
 	{
 		d /= 10;
-		count *= 10;
+		i *= 10;
 	}
-	for (; count >= 1; count /= 10)
+	for (; i >= 1; i /= 10)
 	{
-		_putchar(((m / count) % 10) + 48);
+		_putchar(((m / i) % 10) + 48);
 	}
 }
