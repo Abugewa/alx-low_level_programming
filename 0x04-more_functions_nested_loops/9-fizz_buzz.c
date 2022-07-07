0640c327d8e68e74da54dea8d0c63fe8d4297736
Jesudamilola <damilolaolugbemiga@gmail.com>
2022-07-07 06:54:38 -0700

    fizzbuzz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,41 @@
+#include<stdio.h>
+
+/**
+ * main - fizzbuzz test
+ * print numbers 1 - 100
+ * replace multiples of 3 with Fizz,
+ * multples of 5 with Buzz,
+ * multiples of both with FizzBuzz
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 1; i <= 100; i++)
+		if(i % 3 == 0)
+		{
+			printf("Fizz ");
+		}
+		else if (i % 5 == 0)
+		{
+			printf("Buzz ");
+		}
+		else if ((i % 3 == 0) && (i % 5 ==0))
+		{
+		printf("FizzBuzz ");
+		}
+		else
+		{
+			if (i = 100)
+			{
+			printf("%d", i);
+			}
+			else
+			{
+				printf("%d ", i);
+			}
+		}
+	return (0);
+}
