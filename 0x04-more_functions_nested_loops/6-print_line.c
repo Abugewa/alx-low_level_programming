770f53a633a1583fe8a675d1edc7755283cbb893
Jesudamilola <damilolaolugbemiga@gmail.com>
2022-07-07 05:36:17 -0700

    print a straight line

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
-/** print_line - draws a straight line in the terminal
- * @n: number of times _ is drawn
+/** print_line - draws a straight line
+ * @n:number of times line is drawn
  *
  * Return:no return value
  */
