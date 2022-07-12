302231158ecfb9876f872aea103e15ed47224e4e
Jesudamilola <damilolaolugbemiga@gmail.com>
2022-07-12 06:54:56 -0700

    prints a string then a new line

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _puts - prints a string followed by a new line
+ * @str: string
+ *
+ * Return: nil
+ */
+
+void _puts(char *str)
+{
+	int len;
+
+	len = 0;
+	while (len >= 0)
+	{
+		if (str[len] == '\0')
+		{
+			_putchar(10);
+		}
+		_putchar(str[len]);
+		len++;
+	}
+}
