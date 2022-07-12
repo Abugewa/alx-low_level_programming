51b6f5007608e3b724cd8963a62b60e4f5e7f812
Jesudamilola <damilolaolugbemiga@gmail.com>
2022-07-12 07:53:47 -0700

    print every other character

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * puts2 - prints every other character
+ * @str: input string
+ *
+ * Return: nil
+ */
+
+void puts2(char *str)
+{
+	int len;
+
+	len = 0;
+	while (len >= 0)
+	{
+		if (str[len] == '\0')
+		{
+			_putchar(10);
+			break;
+		}
+		if (len % 2 == 0)
+		{
+			_putchar(str[len]);
+		}
+	}
+}
