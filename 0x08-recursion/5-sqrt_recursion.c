75080daf6db79105a78666e6ef5d9ca8e84303f2
Jesudamilola <damilolaolugbemiga@gmail.com>
2022-07-20 08:53:37 -0700

    square root

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,6 +1,25 @@
 #include "main.h"
 
 /**
+ * nat_sqrt- find natural roots
+ * @n: numvber
+ * @i: counter
+ *
+ * Return: root
+ */
+
+int nat_sqrt(int n, int i)
+{
+	if (i % (n / i) == 0)
+	{
+		if (i * (n / i) == n)
+			return (i);
+		else
+			return (-1);
+	}
+	return (nat_sqrt(n, i + 1));
+}
+/**
  * _sqrt_recursion - returns natural square root
  * @n:number
  *
@@ -9,8 +28,6 @@
 
 int _sqrt_recursion(int n)
 {
-	int i = 2;
-
 	if (n < 0)
 	{
 		return (-1);
@@ -25,14 +42,6 @@ int _sqrt_recursion(int n)
 	}
 	else
 	{
-		if (i % (n / i) == n)
-		{
-			return (i);
-		}
-		else
-		{
-			return (-1);
-			i++;
-		}
+		return (nat_sqrt(n, 2));
 	}
 }
