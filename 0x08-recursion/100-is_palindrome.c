32e07e4a4f33bfcbe7813412c22fbd0f406b9927
Jesudamilola <damilolaolugbemiga@gmail.com>
2022-07-20 09:15:35 -0700

    palindrome

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,36 @@
+#include "main.h"
+#include "_strlen_recursion"
+
+/**
+ * comp - compares characters
+ * @s: string
+ * @i: min iterator
+ * @j: max iterator
+ *
+ * Return: 1 or 0
+ */
+
+int comp(char *s, int i, int j)
+{
+	if (*(s + i) == *(s + j))
+	{
+		if (i == j || i == j + 1)
+			return (1);
+		return (comp(s, i + 1, j - 1));
+	}
+	return (0);
+}
+
+/**
+ * is_palindrome - checks if a string is a palindrome
+ * @s: string
+ *
+ * Return: 1 if s is a palindrome, 0 otherwise
+ */
+
+int is_palindrome(char *s)
+{
+	if (*s == '\0')
+		return (1);
+	return (comp(s, 0, _strlen_recursion(s) - 1));
+}
