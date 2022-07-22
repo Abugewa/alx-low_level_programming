723a755c692d4a6994e5d6ff07f379d8a52f7cf9
Jesudamilola <damilolaolugbemiga@gmail.com>
2022-07-22 11:55:01 -0700

    number of arguements

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - prints number of arguements passed
+ * @argc: number of command line arguements
+ * @argv:array of command line arguements
+ *
+ * REturn: 0
+ */
+
+int main(int argc, char *argv[] __attribute__((unused)))
+{
+	printf("%d\n", argc - 1);
+	return (0);
+}
