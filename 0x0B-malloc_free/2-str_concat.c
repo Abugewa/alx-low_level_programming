50008b96bf987a53e4d7ac5e39ffa4e611215937
Jesudamilola <damilolaolugbemiga@gmail.com>
2022-07-27 06:41:22 -0700

    done

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -32,7 +32,7 @@ char *str_concat(char *s1, char *s2)
 	for (k = 0; k < i; k++)
 		concat[k] = s1[k];
 	end = j;
-	for (j = 0; j <= end; j++)
+	for (j = 0; j <= end; k++, j++)
 		concat[k] = s2[j];
 	return (concat);
 }
