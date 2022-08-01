a636f3065d11cad96437facd8f3446cca2ffd973
Jesudamilola <damilolaolugbemiga@gmail.com>
2022-08-01 06:44:59 -0700

    dog struct

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,22 @@
+#ifndef _DOG_H_
+#define _DOG_H_
+
+
+/**
+ * struct dog - structure that stores things about dogs
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: name of dog owner
+ *
+ * Description: the "dog" structure stores vital information
+ * about the dog or dogs including name age and name of owner
+ */
+
+struct dog
+{
+	char *name;	/* 8 bytes */
+	float age;	/* 4 bytes */
+	char *owner;	/* 8 bytes */
+};
+
+#endif
