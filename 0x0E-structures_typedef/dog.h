ebd63d3059ea21555a2b8cc0b8214e3692d84807
Jesudamilola <damilolaolugbemiga@gmail.com>
2022-08-01 07:42:08 -0700

    struct

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -2,7 +2,7 @@
 #define _DOG_H_
 
 /**
- * dog_t - typedef for struct dog
+ * dog_t - Typedef for struct dog
  */
 
 typedef struct dog dog_t;
@@ -26,7 +26,5 @@ struct dog
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
-dog_t *new_dog(char *name, float age, char *owner);
-void free_dog(dog_t *d);
 
 #endif
