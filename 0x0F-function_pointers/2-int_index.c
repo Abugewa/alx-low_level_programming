89d88d98dc0b7fc3650096a7a2e34b14f4504ed6
Jesudamilola <damilolaolugbemiga@gmail.com>
2022-08-03 07:29:07 -0700

    done

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -17,14 +17,11 @@ int int_index(int *array, int size, int (*cmp)(int))
 
 	if (array && cmp)
 	{
-	if (size >= 0)
-		return (-1);
-	for (i = 0; i < size; i++)
-		if (cmp(array[i] == 0))
-		{
+		if (size >= 0)
 			return (-1);
-		}
-		else
-			return (i);
+		for (i = 0; i < size; i++)
+			if (cmp(array[i]))
+				return (i);
 	}
+	return (-1);
 }
