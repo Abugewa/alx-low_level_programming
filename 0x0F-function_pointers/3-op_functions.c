cb01e165cbfb0eb200c8ae33b01f889b37e239bd
Jesudamilola <damilolaolugbemiga@gmail.com>
2022-08-03 08:31:36 -0700

    functions

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,70 @@
+#include "calc.h"
+
+/**
+ * op_add - returns sum
+ *
+ * @a: first number
+ * @b: second number
+ *
+ * Return: output
+ */
+
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - returns difference
+ *
+ * @a: first number
+ * @b: second number
+ *
+ * Return: output
+ */
+
+int op_sub(int a, int b)
+{
+	return (abs(a - b));
+}
+
+/**
+ * op_mul - returns product
+ * @a: first number
+ * @b: second number
+ *
+ * Return: output
+ */
+
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - returns result of division
+ *
+ * @a: first number
+ * @b: second number
+ *
+ * Return: output
+ */
+
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * op_mod - returns remainder of division
+ *
+ * @a: first number
+ * @b: second number
+ *
+ * Return: output
+ */
+
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
