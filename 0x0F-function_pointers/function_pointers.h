5110023dd4499884c2a475507be2334167653c21
Jesudamilola <damilolaolugbemiga@gmail.com>
2022-08-03 06:26:18 -0700

    header

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -5,7 +5,7 @@
 
 int _putchar(int);
 void print_name(char *name, void (*f)(char *));
-array_iterator(int *array, size_t size, void (*action)(int));
+void array_iterator(int *array, size_t size, void (*action)(int));
 int int_index(int *array, int size, int (*cmp)(int));
 
 #endif
