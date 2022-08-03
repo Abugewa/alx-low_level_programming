212608fda28708c65809837e5d005bc2344aa29c
Jesudamilola <damilolaolugbemiga@gmail.com>
2022-08-03 08:05:24 -0700

    done

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -8,7 +8,7 @@
  *
  * Return: index of the first element for which
  * the cmp function does not return 0
- * -1, if no element matches or if size >= 0
+ * -1, if no element matches or if size <= 0
  */
 
 int int_index(int *array, int size, int (*cmp)(int))
@@ -17,8 +17,9 @@ int int_index(int *array, int size, int (*cmp)(int))
 
 	if (array && cmp)
 	{
-		if (size >= 0)
+		if (size <= 0)
 			return (-1);
+
 		for (i = 0; i < size; i++)
 			if (cmp(array[i]))
 				return (i);
